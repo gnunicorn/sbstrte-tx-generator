9a0db0fc7df0e5a75c8fd85eb5e4e4fbfea25d68
Benjamin Kampmann <ben@gnunicorn.org>
2019-04-01 22:26:43 +0200

    update script

--- tx-gen.js
+++ tx-gen.js
@@ -1,59 +1,51 @@
 const { ApiPromise } = require('@polkadot/api');
+const TestKeyring = require('@polkadot/keyring/testingPairs').default;
 const { Keyring } = require('@polkadot/keyring');
-const { stringToU8a } = require('@polkadot/util');
+const { randomAsU8a } = require('@polkadot/util-crypto');
+const sleep = (millis) => new Promise(_ => setTimeout(_, millis));
 
-const setupAccounts = () => {
-	const accountNames = [
-		"Alice",
-		"Bob",
-		"Charlie",
-		"Dave",
-		"Eve",
-		"Ferdie",
-	];
+async function main () {
+    const keyring = new Keyring();
+    const api = await ApiPromise.create();
+    const testKeyring = TestKeyring();
 
-	const keyring = new Keyring();
+    let nonce = await api.query.system.accountNonce(testKeyring.alice.address());
+    console.log(nonce)
+    let amount = 501;
+    for (let j = 0; j < 100000; j++) {
+        for (let i = 0; i < 11; i++) {
+            const hash = await api.tx.balances
+                .transfer(keyring.encodeAddress(randomAsU8a()), amount)
+                .signAndSend(testKeyring.alice, { nonce });
+           // if (i === 0) console.log(`sent ${hash} with nonce ${nonce}`);
+            nonce = nonce.addn(1);
+        }
+        let [accountNonce, blockPeriod, validators] = await Promise.all([
+            api.query.system.accountNonce(testKeyring.alice.address()),
+            api.query.timestamp.blockPeriod(),
+            api.query.session.validators()
+        ]);
 
-	return accountNames.map(name => {
-		const seed = name.padEnd(32, ' ');
-		return keyring.addFromSeed(stringToU8a(seed));
-	});
-};
-
-const random = n => Math.floor(Math.random() * Math.floor(n));
-
-const sleep = (secs) => new Promise(_ => setTimeout(_, secs * 1000));
-
-const main = async () => {
-	const N_TXS = 10000;
-
-	const api = await ApiPromise.create();
-	const accounts = setupAccounts();
-	const nonces = await Promise.all(accounts.map(async account => {
-		let nonce = await api.query.system.accountNonce(account.address());
-		return Number(nonce);
-	}));
-
-	for (let i = 0; i < N_TXS; i++) {
-		const src = random(accounts.length);
-		const dst = random(accounts.length);
-
-		const srcAcc = accounts[src];
-		const dstAddr = accounts[dst].address();
-
-		const transfer = api.tx.balances.transfer(dstAddr, random(1000));
-
-		try {
-			const hash = await transfer.signAndSend(srcAcc, { nonce: nonces[src] });
-			nonces[src] += 1;
-			console.log('Transfer sent with hash', hash.toHex());
-		} catch {
-		}
-
-		if (i != 0 && i % 100 == 0) {
-			await sleep(4);
-		}
-	}
+        await sleep(280);
+        if (validators && validators.length > 0 ) {
+            const validatorBalances = await Promise.all(
+                validators.map(authorityId =>
+                    api.query.balances.freeBalance(authorityId)
+                )
+            );
+            console.log(`balance: ${validatorBalances[0].toString()}`);
+            console.log(`accountNonce(${testKeyring.alice.address()}) ${accountNonce} - nonce: ${nonce} - diff: ${nonce - accountNonce}`);
+            //console.log(`blockPeriod ${blockPeriod.toNumber()} seconds`);
+            if (nonce.words[0] > accountNonce.words[0] + 220) {
+                while (nonce > accountNonce.words[0]) {
+                    console.log("Sleeping to allow tx catch up....");
+                    await sleep(100);
+                    accountNonce = await api.query.system.accountNonce(testKeyring.alice.address());
+                }
+                nonce = await api.query.system.accountNonce(testKeyring.alice.address());
+                console.log('Reset account nonce to:' + nonce);
+            }
+        }
+    }
 }
-
 main().catch(console.error).finally(() => process.exit());
